44531aef81cfe1950d51423fcc99a0daba4d2e61
Mikhail <>
2025-11-11 18:45:05 +0300

    - заготовка для инсталлера сцены и асмдеф

---
+++ IP_Kuznetsov_2025/Assets/_Project/Scripts/GameScene/Scene/GameSceneInstaller.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using Zenject;
+using ZerglingUnityPlugins.ZenjectExtentions.SceneInstallers;
+
+namespace _Project.Scripts.GameScene.Scene
+{
+    public class GameSceneInstaller : SceneInstaller
+    {
+        protected override void OnInstallBindings()
+        {
+        }
+    }
+}
