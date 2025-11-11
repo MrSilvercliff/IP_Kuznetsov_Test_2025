439ff8e27acd786f023782b64f07948a7611462e
Mikhail <>
2025-11-11 18:12:49 +0300

    - мои плагины

---
+++ IP_Kuznetsov_2025/Assets/Plugins/ZerglingUnityPlugins/Balance_JSON_Object/Scripts/Configs/BalanceConfigBase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Plugins.ZerglingUnityPlugins.Balance_Total_JSON.Scripts.Configs
+{
+    public interface IBalanceConfig
+    {
+        TextAsset BalanceFile { get; }
+
+#if UNITY_EDITOR
+        string GoogleSheetId { get; }
+#endif
+
+        BalanceGoogleSheetPage[] GoogleSheetPages { get; }
+
+        void ParseBalance();
+
+        public enum ParseFuncType
+        {
+            NONE = 0,
+            AsConfig = 1,
+            AsDictionary = 2,
+            Custom = 3,
+        }
+    }
+
+    /// <summary>
+    /// не забудь в дочернем классе добавить
+    /// [CreateAssetMenu(fileName = "T.Name", menuName = "Configs/T.Name")]
+    /// чтобы добавить пункт меню для создания конфига
+    /// </summary>
+    public abstract class BalanceConfigBase : ScriptableObject, IBalanceConfig
+    {
+        public TextAsset BalanceFile => _balanceFile;
+
+#if UNITY_EDITOR
+        public string GoogleSheetId => _googleSheetId;
+#endif
+        public BalanceGoogleSheetPage[] GoogleSheetPages => _googleSheetPages;
+
+        [SerializeField] private TextAsset _balanceFile;
+
+#if UNITY_EDITOR
+        [SerializeField] private string _googleSheetId;
+#endif
+
+        [SerializeField] private BalanceGoogleSheetPage[] _googleSheetPages;
+
+        public abstract void ParseBalance();
+    }
+
+    [Serializable]
+    public class BalanceGoogleSheetPage
+    {
+        public string PageName => _pageName;
+        public Object TargetBalanceStorage => _targetBalanceStorage;
+        public bool DebugPrintOnInit => _debugPrintOnInit;
+        public IBalanceConfig.ParseFuncType ParseFuncType => _parseFuncType;
+
+        [SerializeField] private string _pageName;
+        [SerializeField] private Object _targetBalanceStorage;
+        [SerializeField] private bool _debugPrintOnInit;
+        [SerializeField] private IBalanceConfig.ParseFuncType _parseFuncType;
+    }
+}
