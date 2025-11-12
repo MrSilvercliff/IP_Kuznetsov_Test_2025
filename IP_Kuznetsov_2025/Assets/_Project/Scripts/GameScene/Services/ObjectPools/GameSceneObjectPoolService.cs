9e260e91587871c0ea8c84e4cbdc0f9aa73a1155
Mikhail <>
2025-11-12 20:08:54 +0300

    - прокинул пул виджетов слотов инвентаря в сервис объектных пулов

--- IP_Kuznetsov_2025/Assets/_Project/Scripts/GameScene/Services/ObjectPools/GameSceneObjectPoolService.cs
+++ IP_Kuznetsov_2025/Assets/_Project/Scripts/GameScene/Services/ObjectPools/GameSceneObjectPoolService.cs
@@ -1,5 +1,6 @@
 using _Project.Scripts.GameScene.GameItems;
 using _Project.Scripts.GameScene.Inventory;
+using _Project.Scripts.GameScene.UI.Widgets.Inventory;
 using UnityEngine;
 using Zenject;
 
@@ -10,6 +11,8 @@ namespace _Project.Scripts.GameScene.Services.ObjectPools
         GameItem.Pool GameItemPool { get; }
         InventoryControllerPool InventoryControllerPool { get; }
         InventorySlotController.Pool InventorySlotControllerPool { get; }
+
+        InventorySlotWidget.Pool InventorySlotWidgetPool { get; }
     }
 
     public class GameSceneObjectPoolService : IGameSceneObjectPoolService
@@ -18,8 +21,12 @@ namespace _Project.Scripts.GameScene.Services.ObjectPools
         public InventoryControllerPool InventoryControllerPool => _inventoryControllerPool;
         public InventorySlotController.Pool InventorySlotControllerPool => _inventorySlotControllerPool;
 
+        public InventorySlotWidget.Pool InventorySlotWidgetPool => _inventorySlotWidgetPool;
+
         [Inject] private GameItem.Pool _gameItemPool;
         [Inject] private InventoryControllerPool _inventoryControllerPool;
         [Inject] private InventorySlotController.Pool _inventorySlotControllerPool;
+
+        [Inject] private InventorySlotWidget.Pool _inventorySlotWidgetPool;
     }
 }
