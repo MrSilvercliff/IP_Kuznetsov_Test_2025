eb75ea7dd1e2d4f360d8682dead20583540d7af9
Mikhail <>
2025-11-12 15:36:57 +0300

    - добавил стораджи баланса инветаря и предметов в баланс сервис проекта

--- IP_Kuznetsov_2025/Assets/_Project/Scripts/Project/Services/Balance/ProjectBalanceService.cs
+++ IP_Kuznetsov_2025/Assets/_Project/Scripts/Project/Services/Balance/ProjectBalanceService.cs
@@ -1,3 +1,4 @@
+using _Project.Scripts.Project.Services.Balance.Storages;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,17 +9,28 @@ namespace _Project.Scripts.Project.Services.Balance
 {
     public interface IProjectBalanceService : IBalanceServiceAbstractAsync
     {
+        IInventoryBalanceStorage Inventory { get; }
+        IGameItemBalanceStorage GameItems { get; }
     }
 
     public class ProjectBalanceService : BalanceServiceAbstractAsync, IProjectBalanceService
     {
+        public IInventoryBalanceStorage Inventory { get; }
+        public IGameItemBalanceStorage GameItems { get; }
+
         public ProjectBalanceService() 
         {
+            Inventory = new InventoryBalanceStorage();
+            GameItems = new GameItemBalanceStorage();
         }
 
         protected override HashSet<IProjectService> GetStoragesToInit()
         {
             var result = new HashSet<IProjectService>();
+
+            result.Add(Inventory);
+            result.Add(GameItems);
+
             return result;
         }
     }
