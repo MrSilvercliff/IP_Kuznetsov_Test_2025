6c50726ef8eaf3681822f0cc09544f9930cab614
Mikhail <>
2025-11-12 20:10:04 +0300

    - биндинги всякого от игровой сцены

--- IP_Kuznetsov_2025/Assets/_Project/Scripts/GameScene/Scene/GameSceneInstaller.cs
+++ IP_Kuznetsov_2025/Assets/_Project/Scripts/GameScene/Scene/GameSceneInstaller.cs
@@ -1,3 +1,10 @@
+using _Project.Scripts.GameScene.GameItems;
+using _Project.Scripts.GameScene.Inventory;
+using _Project.Scripts.GameScene.Services.Inventory;
+using _Project.Scripts.GameScene.Services.ObjectPools;
+using _Project.Scripts.GameScene.Services.Player;
+using _Project.Scripts.GameScene.UI.Widgets.Inventory;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using Zenject;
 using ZerglingUnityPlugins.ZenjectExtentions.SceneInstallers;
@@ -6,8 +13,73 @@ namespace _Project.Scripts.GameScene.Scene
 {
     public class GameSceneInstaller : SceneInstaller
     {
+        [SerializeField] private GameSceneObjectPoolContainer _objectPoolContainer;
+
         protected override void OnInstallBindings()
         {
+            BindConfigs();
+
+            BindObjectPools();
+
+            BindPlayerServices();
+
+            BindInventoryServices();
+
+            BindServiceIniter();
+        }
+
+        private void BindServiceIniter()
+        {
+            Container.Bind<IGameSceneServiceIniter>().To<GameSceneServiceIniter>().AsSingle();
+        }
+
+        private void BindConfigs()
+        { 
+        }
+
+        private void BindObjectPools()
+        { 
+            BindNonMonoPools();
+            BindMonoPools();
+
+            Container.Bind<IGameSceneObjectPoolService>().To<GameSceneObjectPoolService>().AsSingle();
+        }
+
+        private void BindNonMonoPools()
+        {
+            Container.BindMemoryPool<GameItem, GameItem.Pool>();
+            Container.BindMemoryPool<InventoryController, InventoryControllerPool>();
+            Container.BindMemoryPool<InventorySlotController, InventorySlotController.Pool>();
+        }
+
+        private void BindMonoPools()
+        { 
+            BindInventorySlotWidgetPool();
+        }
+
+        private void BindInventorySlotWidgetPool()
+        {
+            var objectPoolItem = _objectPoolContainer.InventorySlotWidget;
+
+            var prefab = objectPoolItem.Prefab;
+            var container = objectPoolItem.Container;
+            var initSize = objectPoolItem.PoolInitialSize;
+
+            Container.BindMemoryPool<InventorySlotWidget, InventorySlotWidget.Pool>()
+                .WithInitialSize(initSize)
+                .FromComponentInNewPrefab(prefab)
+                .UnderTransform(container);
+        }
+
+        private void BindPlayerServices()
+        { 
+            Container.Bind<IPlayerInventoryService>().To<PlayerInventoryService>().AsSingle();
+            Container.Bind<IPlayerService>().To<PlayerService>().AsSingle();
+        }
+
+        private void BindInventoryServices()
+        { 
+            Container.Bind<IInventoryService>().To<InventoryService>().AsSingle();
         }
     }
 }
--- IP_Kuznetsov_2025/Assets/_Project/Scripts/GameScene/Scene/GameSceneObjectPoolContainer.cs
+++ IP_Kuznetsov_2025/Assets/_Project/Scripts/GameScene/Scene/GameSceneObjectPoolContainer.cs
@@ -1,8 +1,12 @@
+using _Project.Scripts.Project.ObjectPools;
 using UnityEngine;
 
 namespace _Project.Scripts.GameScene.Scene
 {
     public class GameSceneObjectPoolContainer : MonoBehaviour
     {
+        public ObjectPoolContainerItem InventorySlotWidget => _inventorySlotWidget;
+
+        [SerializeField] private ObjectPoolContainerItem _inventorySlotWidget;
     }
 }
