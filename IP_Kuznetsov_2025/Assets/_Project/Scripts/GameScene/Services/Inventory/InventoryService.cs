d07cea6fc49782c07a7876d64c1d9bf0bbdfe0dd
Mikhail <>
2025-11-13 17:52:35 +0300

    - делаю заполнение инвентаря рандомными предметами

--- IP_Kuznetsov_2025/Assets/_Project/Scripts/GameScene/Services/Inventory/InventoryService.cs
+++ IP_Kuznetsov_2025/Assets/_Project/Scripts/GameScene/Services/Inventory/InventoryService.cs
@@ -8,10 +8,16 @@ namespace _Project.Scripts.GameScene.Services.Inventory
 {
     public interface IInventoryService : IProjectService
     {
+        void FillRandom(IInventoryController targetInventory);
+        void ClearInventory(IInventoryController inventoryController);
     }
 
     public class InventoryService : IInventoryService
     {
+        [Inject] private IInventoryItemAddService _itemAddService;
+        [Inject] private IInventoryItemRemoveService _itemRemoveService;
+        [Inject] private IInventorySlotService _inventorySlotService;
+
         public async Task<bool> Init()
         {
             return true;
@@ -20,6 +26,16 @@ namespace _Project.Scripts.GameScene.Services.Inventory
         public bool Flush()
         {
             return true;
+        }
+
+        public void FillRandom(IInventoryController targetInventory)
+        {
+            _itemAddService.FillRandom(targetInventory);
+        }
+
+        public void ClearInventory(IInventoryController inventoryController)
+        {
+            _itemRemoveService.ClearInventory(inventoryController);
         }
     }
 }
