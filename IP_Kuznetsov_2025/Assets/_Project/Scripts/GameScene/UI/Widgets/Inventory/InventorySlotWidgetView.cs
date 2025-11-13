bd0b6b4da8f14bee8a2d6c78de66d270d8feb249
Mikhail <>
2025-11-13 18:57:10 +0300

    - добавил установку иконку во вьюху слота инвентаря

--- IP_Kuznetsov_2025/Assets/_Project/Scripts/GameScene/UI/Widgets/Inventory/InventorySlotWidgetView.cs
+++ IP_Kuznetsov_2025/Assets/_Project/Scripts/GameScene/UI/Widgets/Inventory/InventorySlotWidgetView.cs
@@ -1,7 +1,9 @@
 using _Project.Scripts.GameScene.Inventory;
+using _Project.Scripts.Project.Configs;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Zenject;
 
 namespace _Project.Scripts.GameScene.UI.Widgets.Inventory
 {
@@ -14,6 +16,8 @@ namespace _Project.Scripts.GameScene.UI.Widgets.Inventory
         [SerializeField] private GameObject _fullState;
         [SerializeField] private Image _iconImage;
         [SerializeField] private TMP_Text _countText;
+
+        [Inject] private IProjectSpriteConfig _spriteConfig;
 
         public void Refresh(IInventorySlotController inventorySlotController)
         {
@@ -31,11 +35,12 @@ namespace _Project.Scripts.GameScene.UI.Widgets.Inventory
 
         private void ShowFullState(IInventorySlotController inventorySlotController)
         {
-            _emptyState.SetActive(true);
-            _fullState.SetActive(false);
+            _emptyState.SetActive(false);
+            _fullState.SetActive(true);
 
             var slotItem = inventorySlotController.Item;
 
+            _iconImage.sprite = _spriteConfig.GetGameItemIcon(slotItem.IconId);
             _countText.gameObject.SetActive(slotItem.IsStackable);
             _countText.text = slotItem.Count.ToString();
         }
