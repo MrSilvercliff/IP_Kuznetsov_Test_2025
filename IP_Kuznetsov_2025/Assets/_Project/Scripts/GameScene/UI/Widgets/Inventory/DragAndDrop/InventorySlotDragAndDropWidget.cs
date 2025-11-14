8c1757854dca6b3c0a218cf64988adc9cc7c534d
Mikhail <>
2025-11-14 17:06:06 +0300

    - делаю виджет драг-н-дропа

---
+++ IP_Kuznetsov_2025/Assets/_Project/Scripts/GameScene/UI/Widgets/Inventory/DragAndDrop/InventorySlotDragAndDropWidget.cs
@@ -0,0 +1,80 @@
+using _Project.Scripts.GameScene.DragAndDrop;
+using _Project.Scripts.GameScene.Inventory;
+using UnityEngine;
+using Zenject;
+using ZerglingUnityPlugins.Tools.Scripts.Mono;
+
+namespace _Project.Scripts.GameScene.UI.Widgets.Inventory.DragAndDrop
+{
+    public class InventorySlotDragAndDropWidget : MonoBehaviour, IMonoUpdatable, IInventorySlotDragAndDropListener
+    {
+        [SerializeField] private InventorySlotDragAndDropWidgetView _view;
+
+        [Inject] private IMonoUpdater _monoUpdater;
+        [Inject] private IDragAndDropController _dragAndDropController;
+
+        private bool _dragInProcess;
+        private bool _dropInProcess;
+
+        private IInventorySlotController _dragInventorySlotController;
+        private IInventorySlotController _dropInventorySlotController;
+
+        private void OnEnable()
+        {
+            _monoUpdater.Subscribe(this);
+            _dragAndDropController.Subscribe(this);
+        }
+
+        private void OnDisable()
+        {
+            _monoUpdater.UnSubscribe(this);
+            _dragAndDropController.UnSubscribe(this);
+        }
+
+        public void Init()
+        {
+            _dragInProcess = false;
+        }
+
+        public void OnUpdate(float deltaTime)
+        {
+            if (_dragInProcess)
+                return;
+
+            if (_dropInProcess)
+                return;
+
+            _view.OnUpdate(deltaTime);
+        }
+
+        public void OnDrag(IInventorySlotController inventorySlotController)
+        {
+            if (_dropInProcess)
+                return;
+
+            _dragInProcess = true;
+            _dragInventorySlotController = inventorySlotController;
+            _view.OnDragStart(inventorySlotController);
+        }
+
+        public void OnDrop(IInventorySlotController inventorySlotController)
+        {
+            if (_dropInProcess)
+                return;
+
+            _dropInProcess = true;
+
+            _dragInProcess = false;
+            _view.OnDragStop();
+
+            _dropInventorySlotController = inventorySlotController;
+            ProcessDrop();
+
+            _dropInProcess = false;
+        }
+
+        private void ProcessDrop()
+        { 
+        }
+    }
+}
---
+++ IP_Kuznetsov_2025/Assets/_Project/Scripts/GameScene/UI/Widgets/Inventory/DragAndDrop/InventorySlotDragAndDropWidgetView.cs
@@ -0,0 +1,49 @@
+using _Project.Scripts.GameScene.Input;
+using _Project.Scripts.GameScene.Inventory;
+using _Project.Scripts.GameScene.UI.Widgets.Inventory;
+using UnityEngine;
+using Zenject;
+using ZerglingUnityPlugins.Tools.Scripts.Mono;
+
+namespace _Project.Scripts.GameScene.UI.Widgets.Inventory.DragAndDrop
+{
+    public class InventorySlotDragAndDropWidgetView : MonoBehaviour, IMonoUpdatable
+    {
+        [SerializeField] private RectTransform _rectTransform;
+        [SerializeField] private RectTransform _draggableRectTransform;
+        [SerializeField] private InventorySlotWidget _draggedWidget;
+
+        [Inject] private IInputController _inputController;
+
+        private Vector2 _ratioMultiplier; // ratio between canvas rect size and screen size
+
+        public void Init()
+        {
+            var screen = new Vector2(Screen.width, Screen.height);
+            _ratioMultiplier = new Vector2(_rectTransform.rect.width / screen.x, _rectTransform.rect.height / screen.y);
+        }
+
+        public void OnDragStart(IInventorySlotController inventorySlotController)
+        {
+            _draggedWidget.Setup(inventorySlotController);
+            _draggedWidget.SetActive(true);
+        }
+
+        public void OnDragStop()
+        { 
+            _draggedWidget.SetActive(false);
+            _draggedWidget.Setup(null);
+        }
+
+        public void OnUpdate(float deltaTime)
+        {
+            var pointerPosition = _inputController.PointerPosition;
+            
+            var anchoredX = pointerPosition.x * _ratioMultiplier.x;
+            var anchoredY = pointerPosition.y * _ratioMultiplier.y;
+            var anchoredPosition = new Vector2(anchoredX, anchoredY);
+            
+            _draggableRectTransform.anchoredPosition = anchoredPosition;
+        }
+    }
+}
