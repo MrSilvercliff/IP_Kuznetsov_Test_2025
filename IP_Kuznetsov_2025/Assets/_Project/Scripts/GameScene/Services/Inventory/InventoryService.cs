117437e836ea4386a708e46626624b27c5c6513d
Mikhail <>
2025-11-14 20:55:56 +0300

    - добавил вызов перемещения предметов между слотами в главнйы сервис инвентаря

--- IP_Kuznetsov_2025/Assets/_Project/Scripts/GameScene/Services/Inventory/InventoryService.cs
+++ IP_Kuznetsov_2025/Assets/_Project/Scripts/GameScene/Services/Inventory/InventoryService.cs
@@ -12,12 +12,14 @@ namespace _Project.Scripts.GameScene.Services.Inventory
         void FillRandom(IInventoryController targetInventory);
         void ClearInventory(IInventoryController inventoryController);
         bool ClearInventorySlot(IInventorySlotController inventorySlotController);
+        void MoveItemBetweenSlots(IInventorySlotController fromInventorySlotController, IInventorySlotController toInventorySlotController);
     }
 
     public class InventoryService : IInventoryService
     {
         [Inject] private IInventoryItemAddService _itemAddService;
         [Inject] private IInventoryItemRemoveService _itemRemoveService;
+        [Inject] private IInventoryItemMoveService _itemMoveService;
         [Inject] private IInventorySlotService _inventorySlotService;
 
         public async Task<bool> Init()
@@ -44,6 +46,11 @@ namespace _Project.Scripts.GameScene.Services.Inventory
         {
             var result = _itemRemoveService.ClearInventorySlot(inventorySlotController);
             return result;
+        }
+
+        public void MoveItemBetweenSlots(IInventorySlotController fromInventorySlotController, IInventorySlotController toInventorySlotController)
+        {
+            _itemMoveService.MoveItemBetweenSlots(fromInventorySlotController, toInventorySlotController);
         }
     }
 }
