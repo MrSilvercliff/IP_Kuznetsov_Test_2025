2959a7945d5d4de76086a50d14927cef842a30f0
Mikhail <>
2025-11-14 21:10:53 +0300

    - сделал мердж и свап предметов при перетаскивании

--- IP_Kuznetsov_2025/Assets/_Project/Scripts/GameScene/Services/Inventory/InventoryItemMoveService.cs
+++ IP_Kuznetsov_2025/Assets/_Project/Scripts/GameScene/Services/Inventory/InventoryItemMoveService.cs
@@ -1,4 +1,6 @@
+using _Project.Scripts.GameScene.GameItems;
 using _Project.Scripts.GameScene.Inventory;
+using _Project.Scripts.GameScene.Services.ObjectPools;
 using _Project.Scripts.GameScene.UI.Events;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -16,6 +18,7 @@ namespace _Project.Scripts.GameScene.Services.Inventory
     public class InventoryItemMoveService : IInventoryItemMoveService
     {
         [Inject] private IEventBusAsync _eventBus;
+        [Inject] private IGameSceneObjectPoolService _objectPoolService;
 
         public Task<bool> Init()
         {
@@ -29,27 +32,23 @@ namespace _Project.Scripts.GameScene.Services.Inventory
 
         public void MoveItemBetweenSlots(IInventorySlotController fromInventorySlotController, IInventorySlotController toInventorySlotController)
         {
+            if (fromInventorySlotController == toInventorySlotController)
+                return;
+
             if (toInventorySlotController.IsEmpty)
             {
                 MoveItemToEmptySlot(fromInventorySlotController, toInventorySlotController);
-                return;
             }
-
-            // toInventorySlot not empty
-            var fromSlotItem = fromInventorySlotController.Item;
-            var toSlotItem = toInventorySlotController.Item;
-
-            if (fromSlotItem.Id == toSlotItem.Id)
-                MergeItemsInSlot(fromInventorySlotController, toInventorySlotController);
             else
-                SwapItemsInSlots(fromInventorySlotController, toInventorySlotController);
-        }
+            {
+                var fromSlotItem = fromInventorySlotController.Item;
+                var toSlotItem = toInventorySlotController.Item;
 
-        private void MoveItemToEmptySlot(IInventorySlotController fromInventorySlotController, IInventorySlotController toInventorySlotController)
-        {
-            var fromSlotItem = fromInventorySlotController.Item;
-            fromInventorySlotController.SetItem(null);
-            toInventorySlotController.SetItem(fromSlotItem);
+                if (fromSlotItem.Id == toSlotItem.Id && fromSlotItem.IsStackable)
+                    MergeItemsInSlot(fromInventorySlotController, toInventorySlotController);
+                else
+                    SwapItemsInSlots(fromInventorySlotController, toInventorySlotController);
+            }
 
             var evntFrom = new InventorySlotChangedEvent(fromInventorySlotController);
             _eventBus.Fire(evntFrom);
@@ -58,12 +57,54 @@ namespace _Project.Scripts.GameScene.Services.Inventory
             _eventBus.Fire(evntTo);
         }
 
+        private void MoveItemToEmptySlot(IInventorySlotController fromInventorySlotController, IInventorySlotController toInventorySlotController)
+        {
+            var fromSlotItem = fromInventorySlotController.Item;
+            fromInventorySlotController.SetItem(null);
+            toInventorySlotController.SetItem(fromSlotItem);
+        }
+
         private void MergeItemsInSlot(IInventorySlotController fromInventorySlotController, IInventorySlotController toInventorySlotController)
-        { 
+        {
+            var fromSlotItem = fromInventorySlotController.Item;
+            var fromSlotItemCount = fromSlotItem.Count;
+
+            var toSlotItem = toInventorySlotController.Item;
+            var toSlotItemCount = toSlotItem.Count;
+
+            var stackSize = toSlotItem.StackSize;
+            var itemsToMove = stackSize - toSlotItemCount;
+
+            if (itemsToMove >= fromSlotItemCount)
+            {
+                // merging stacks with emptying "from slot"
+
+                fromInventorySlotController.SetItem(null);
+                toSlotItemCount += fromSlotItemCount;
+                toSlotItem.SetCount(toSlotItemCount);
+                ReturnGameItemToPool(fromSlotItem);
+                return;
+            }
+
+            // merging stacks without emptying "from slot"
+            toSlotItem.SetCount(stackSize);
+            fromSlotItemCount -= itemsToMove;
+            fromSlotItem.SetCount(fromSlotItemCount);
         }
 
         private void SwapItemsInSlots(IInventorySlotController fromInventorySlotController, IInventorySlotController toInventorySlotController)
-        { 
+        {
+            var fromSlotItem = fromInventorySlotController.Item;
+            var toSlotItem = toInventorySlotController.Item;
+
+            fromInventorySlotController.SetItem(toSlotItem);
+            toInventorySlotController.SetItem(fromSlotItem);
+        }
+
+        private void ReturnGameItemToPool(IGameItem gameItem)
+        {
+            var gameItemPool = _objectPoolService.GameItemPool;
+            gameItemPool.Despawn((GameItem)gameItem);
         }
     }
 }
