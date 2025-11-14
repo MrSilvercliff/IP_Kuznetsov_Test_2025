4e5cc40b8348b92ae2b04b7a55995a09442c554e
Mikhail <>
2025-11-14 18:05:33 +0300

    - бинд объектного пула драггабл слотов инвентаря и конфига драг-н-дропа

--- IP_Kuznetsov_2025/Assets/_Project/Scripts/GameScene/Scene/GameSceneObjectPoolContainer.cs
+++ IP_Kuznetsov_2025/Assets/_Project/Scripts/GameScene/Scene/GameSceneObjectPoolContainer.cs
@@ -6,7 +6,9 @@ namespace _Project.Scripts.GameScene.Scene
     public class GameSceneObjectPoolContainer : MonoBehaviour
     {
         public ObjectPoolContainerItem InventorySlotWidget => _inventorySlotWidget;
+        public ObjectPoolContainerItem InventorySlotDraggableWidget => _inventorySlotDraggableWidget;
 
         [SerializeField] private ObjectPoolContainerItem _inventorySlotWidget;
+        [SerializeField] private ObjectPoolContainerItem _inventorySlotDraggableWidget;
     }
 }
