34769af6d7fdd7873dfc12631030bead3857c169
Mikhail <>
2025-11-15 14:17:14 +0300

    - сделал виджет тултипа для инвентарных слотов

---
+++ IP_Kuznetsov_2025/Assets/_Project/Scripts/GameScene/UI/Widgets/Inventory/InventorySlot/InventorySlotTooltipWidget.cs
@@ -0,0 +1,47 @@
+using _Project.Scripts.GameScene.DragAndDrop;
+using _Project.Scripts.GameScene.Inventory;
+using _Project.Scripts.GameScene.Services.Tooltip;
+using _Project.Scripts.Project.ObjectPools;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Zenject;
+
+namespace _Project.Scripts.GameScene.UI.Widgets.Inventory.InventorySlot
+{
+    public class InventorySlotTooltipWidget : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField] private Transform _tooltipAnchor;
+
+        [Inject] private IDragAndDropController _dragAndDropController;
+        [Inject] private ITooltipService _tooltipService;
+
+        private IInventorySlotController _inventorySlotController;
+
+        public void Setup(IInventorySlotController inventorySlotController)
+        { 
+            _inventorySlotController = inventorySlotController;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (_dragAndDropController.DragInProcess)
+                return;
+
+            if (_inventorySlotController.IsEmpty)
+                return;
+
+            _tooltipService.ShowGameItemTooltip(_inventorySlotController.Item, _tooltipAnchor.position);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (_dragAndDropController.DragInProcess)
+                return;
+
+            if (_inventorySlotController.IsEmpty)
+                return;
+
+            _tooltipService.HideGameItemTooltip(_inventorySlotController.Item, _tooltipAnchor.position);
+        }
+    }
+}
